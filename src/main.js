1cf5e382d806ae78652afec103a52fe4cf33c000
datastorysurvey <203820840+markscanning@users.noreply.github.com>
2025-09-12 10:59:26 +0200

    webpage branch for website

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-
-createApp(App).mount('#app')
